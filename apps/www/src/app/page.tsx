0970fa54d61b115306aca2d599e5a6c809f5cea8
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2025-10-17 14:52:34 +0100

    dummy fix

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -10,8 +10,7 @@ export default function Home() {
       <Header className="sticky top-0 z-10" />
       <div className="flex-1 flex flex-col pt-4 overflow-y-scroll min-h-0">
         <div className="flex-1 flex flex-col space-y-4 py-4">
-          <HeroSection className="mx-4 min-h-56 md:max-h-96 md:mx-8" />
-          {/* <HeroSection className="mx-4 min-h-56 md:max-h-96 md:mx-8 xl:w-7xl" /> */}
+          <HeroSection className="mx-4 min-h-56 md:max-h-100 md:mx-8" />
           <ProductList className="px-4 md:px-8" />
           <div className="flex-1 lt-xl:flex lt-xl:flex-col lt-xl:space-y-4 xl:grid xl:grid-cols-2 xl:gap-4">
             <OnchainAnalytic className="lt-xl:px-4 xl:flex-1 xl:ml-4 xl:md:ml-8" />
